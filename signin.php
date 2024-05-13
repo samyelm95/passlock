94885f7cdeccab9aaa01ecc193718d88b6288724
samyelm95 <153598667+samyelm95@users.noreply.github.com>
2024-05-13 18:49:55 +0200

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <style>
+        input {
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <form method="POST" action="traitement.php">
+        <label for="nom">Votre nom</label>
+        <input type="text" id="nom" name="nom" placeholder="Entrez votre nom">
+        <br />
+        <label for="prenom">Votre prénom</label>
+        <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prénom">
+        <br />
+        <label for="pseudo">Votre pseudo</label>
+        <input type="text" id="pseudo" name="pseudo" placeholder="Entrez votre pseudo">
+        <br />
+        <label for="email">Votre e-mail</label>
+        <input type="email" id="email" name="email" placeholder="Entrez votre e-mail">
+        <br />
+        <label for="pass">Votre mot de passe</label>
+        <input type="password" id="pass" name="pass" placeholder="Entrez votre mot de passe">
+        <br />
+        <input type="submit" value="M'inscrire" name="ok">
+    </form>
+</body>
+</html>
