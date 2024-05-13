6bd19cb1496e0380ed9e7d627cf8eb22ee3f3526
samyelm95 <153598667+samyelm95@users.noreply.github.com>
2024-05-13 18:52:56 +0200

    Update signin.php

--- signin.php
+++ signin.php
@@ -14,19 +14,19 @@
 <body>
     <form method="POST" action="traitement.php">
         <label for="nom">Votre nom</label>
-        <input type="text" id="nom" name="nom" placeholder="Entrez votre nom">
+        <input type="text" id="nom" name="nom" placeholder="Entrez votre nom" required>
         <br />
         <label for="prenom">Votre prénom</label>
-        <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prénom">
+        <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prénom" required>
         <br />
         <label for="pseudo">Votre pseudo</label>
-        <input type="text" id="pseudo" name="pseudo" placeholder="Entrez votre pseudo">
+        <input type="text" id="pseudo" name="pseudo" placeholder="Entrez votre pseudo" required>
         <br />
         <label for="email">Votre e-mail</label>
-        <input type="email" id="email" name="email" placeholder="Entrez votre e-mail">
+        <input type="email" id="email" name="email" placeholder="Entrez votre e-mail" required>
         <br />
         <label for="pass">Votre mot de passe</label>
-        <input type="password" id="pass" name="pass" placeholder="Entrez votre mot de passe">
+        <input type="password" id="pass" name="pass" placeholder="Entrez votre mot de passe" required>
         <br />
         <input type="submit" value="M'inscrire" name="ok">
     </form>
